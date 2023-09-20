b3192b3001dec203bf66d72e294c8b7e1c695de3
idcletitbesmb <143685986+idcletitbesmb@users.noreply.github.com>
2023-09-20 16:51:40 +0400

    Update + comments

--- scenery.py
+++ scenery.py
@@ -42,18 +42,17 @@ def main():
     height = 0 #the height of the drawn part of the cake 
     layers_count = 0 #number of drawn layers
     while height < cake_height:
+            color = input('Input the color of the next layer: ') #user sets color for new layer
             if layers_count == 4: # we decided to set the maximum number of layers for 5, so if on this if-check the number of layers is 4, it means that there is only one left
-                color = input('Input the color of the last layer: ')
-                layer(cake_height - height, cake_widht, color)
+                layer(cake_height - height, cake_widht, color) #draws a layer of the remaining height
                 break
-            else: 
-                color = input('Input the color of the next layer: ')
-                new_layer_height = int(input('Input the height of the next layer from 1 to ' + str(cake_height - height) + ': '))
+            else:  
+                new_layer_height = int(input('Input the height of the next layer from 1 to ' + str(cake_height - height) + ': ')) #user inputs height of the next layer from 1 to maximum remaining
                 while input_validity_check(new_layer_height, cake_height, height) == False: #checks if input is valid. If not, asks a new input until it is valid
                     new_layer_height = int(input('Too much, try again: '))
                 height += new_layer_height #if input is valid, then the height of the drawn part increases by the height of the new layer
                 layers_count += 1 
-                layer(new_layer_height, cake_widht, color) #draws the new layer
+                layer(new_layer_height, cake_widht, color) #draws a new layer
     update()
     exitonclick()
 
